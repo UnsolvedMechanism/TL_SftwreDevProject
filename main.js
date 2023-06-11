ce5a5494a64eece454bb6e5c8496f0c0f1b2c213
UnsolvedMechanism <124121107+UnsolvedMechanism@users.noreply.github.com>
2023-06-11 18:08:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ import * as THREE from 'three';
 
 import Stats from 'three/addons/libs/stats.module.js';
 
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { RectAreaLightHelper } from 'three/addons/helpers/RectAreaLightHelper.js';
-import { RectAreaLightUniformsLib } from 'three/addons/lights/RectAreaLightUniformsLib.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { OrbitControls } from './OrbitControls.js';
+import { RectAreaLightHelper } from './RectAreaLightHelper.js';
+import { RectAreaLightUniformsLib } from './RectAreaLightUniformsLib.js';
+import { GLTFLoader } from './GLTFLoader.js';
+import { DRACOLoader } from './DRACOLoader.js';
+
 
 let renderer, scene, camera, stats;
 
@@ -27,21 +28,38 @@ function init() {
 
     RectAreaLightUniformsLib.init();
 
-    const rectLight1 = new THREE.RectAreaLight(0xff0000, 75, 40, 100);
+    const ambLight = new THREE.AmbientLight(0xffff00,0.15);
+    scene.add(ambLight);
+
+    const light = new THREE.PointLight(0xff00ff);
+    light.position.set(0, 100, 50);
+    scene.add(light);
+
+    const rectLight0 = new THREE.RectAreaLight(0xffff00, 10, 40, 100);
+    rectLight0.position.set(-100, 50, 200);
+    scene.add(rectLight0);
+
+    const rectLight1 = new THREE.RectAreaLight(0xff0000, 10, 40, 100);
     rectLight1.position.set(-50, 50, 200);
     scene.add(rectLight1);
 
-    const rectLight2 = new THREE.RectAreaLight(0x00ff00, 75, 40, 100);
+    const rectLight2 = new THREE.RectAreaLight(0x00ff00, 10, 40, 100);
     rectLight2.position.set(0, 50, 200);
     scene.add(rectLight2);
 
-    const rectLight3 = new THREE.RectAreaLight(0x0000ff, 75, 40, 100);
+    const rectLight3 = new THREE.RectAreaLight(0x0000ff, 10, 40, 100);
     rectLight3.position.set(50, 50, 200);
     scene.add(rectLight3);
 
+    const rectLight4 = new THREE.RectAreaLight(0x00ffff, 10, 40, 100);
+    rectLight4.position.set(100, 50, 200);
+    scene.add(rectLight4);
+
+    scene.add(new RectAreaLightHelper(rectLight0));
     scene.add(new RectAreaLightHelper(rectLight1));
     scene.add(new RectAreaLightHelper(rectLight2));
     scene.add(new RectAreaLightHelper(rectLight3));
+    scene.add(new RectAreaLightHelper(rectLight4));
     const geoFloor = new THREE.BoxGeometry(2000, 0.1, 2000);
     const matStdFloor = new THREE.MeshStandardMaterial({ color: 0xbcbcbc, roughness: 0.1, metalness: 0 });
     const mshStdFloor = new THREE.Mesh(geoFloor, matStdFloor);
